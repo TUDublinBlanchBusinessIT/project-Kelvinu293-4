e184674ed261caee3b3e4c3fd2c4bd0006d30009
Kelvinu293 <b00141682@mytudublin.ie>
2025-12-02 03:04:47 +0000

    Login page

---
+++ Login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="Login.php" method="POST">
+        Username:<input type="text" name="Username">
+        Password:<input type="text" name="Password">
+        <input type="submit" name="Login" value="Login">
+    </form>
+</body>
+</html>
