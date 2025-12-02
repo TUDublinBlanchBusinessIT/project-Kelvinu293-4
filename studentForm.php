de1ab2930782b23566a35ef96b83503fb9a0e8c3
Kelvinu293 <b00141682@mytudublin.ie>
2025-12-02 02:31:21 +0000

    adding inserting data into MySQL workbench

--- studentForm.php
+++ studentForm.php
@@ -1,12 +1,21 @@
 <?PHP
+include("dbcon.php");
 
-$X = $_GET['Firstname'];
-$Y = $_GET['Lastname'];
-$Z = $_GET['Email'];
-$W = $_GET['DateofBirth'];
+$X = $_POST['Firstname'];    
+$Y = $_POST['Lastname'];     
+$Z = $_POST['Email'];       
+$W = $_POST['DateOfBirth'];
+
+$sql = "INSERT INTO students (Firstname, Lastname, Email, DateOfBirth) VALUES ('$X', '$Y', '$Z', '$W')";
+
+if (mysqli_query($conn, $sql)) {
+  echo "Student added";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 
 
-echo "Student Name:" . $X . " " . $Y;
-echo "Email: " . $Z;
-echo " Date of Birth: ". $W;
+
 ?>
