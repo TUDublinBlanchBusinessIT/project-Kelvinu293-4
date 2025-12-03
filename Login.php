0e6b6d63b7162c4f753a71911145b0eccf09fff6
Kelvinu293 <b00141682@mytudublin.ie>
2025-12-03 01:48:29 +0000

    add more to login

--- Login.php
+++ Login.php
@@ -14,11 +14,25 @@
 </body>
 </html>
 <?php
-$_SESSION["Username"] = "Admin5";
-$_SESSION["Password"] = "Holyfamily";
+if(isset($_POST["Login"])){
+    if(!empty($_POST["Username"]) &&
+       !empty($_POST["Password"])){
+        
+        $_SESSION["Username"] = $_POST["Username"];
+        $_SESSION["Password"] = $_POST["Password"];
+            
+            echo $_SESSION["Username"] . "<br>";
+            echo $_SESSION["Password"] . "<br>";
 
-echo $_SESSION["Username"] . "<br>";
-echo $_SESSION["Password"] . "<br>";
+       }
+
+
+
+
+
+
+}
+
 
 
 
