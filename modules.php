3f9e393683ce4fec43dad099b95eb514d192aa94
Kelvinu293 <b00141682@mytudublin.ie>
2025-12-05 01:36:36 +0000

    module.php start

---
+++ modules.php
@@ -0,0 +1,26 @@
+<?PHP
+session_start();
+include("dbcon.php");
+
+$ModuleCode  = $_POST['ModuleCode'];
+$ModuleName  = $_POST['ModuleName'];
+$Credits     = $_POST['Credits'];
+$Semester    = $_POST['Semester'];
+
+
+
+$sql = "INSERT INTO modules (ModuleCode, ModuleName, Credits, Semester) VALUES ('$ModuleCode', '$ModuleName', $Credits, '$Semester')";
+
+
+if (mysqli_query($conn, $sql)) {
+  echo "form submit";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+
+
+
+?>
