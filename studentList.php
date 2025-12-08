f2db1e448d47a9c16cb90e97656b9e7e1931515a
Kelvinu293 <b00141682@mytudublin.ie>
2025-12-08 03:05:03 +0000

    studentList.php table

--- studentList.php
+++ studentList.php
@@ -1,1 +1,23 @@
+<?php
+
+include("dbcon.php");
+
+$sql = "SELECT programmes.ProgrammeCode, programmes.ProgrammeName, programmes.Mode, modules.ModuleName, modules.Credits, modules.Semester FROM programmes
+INNER JOIN modules ON
+programmes.ProgrammeID = modules.ProgrammeID";
+
+$result = mysqli_query($conn,$sql);
+while($row=mysqli_fetch_assoc($result)) {
+    $programmecode = $row['ProgrammeCode'];
+    $programmename = $row['ProgrammeName'];
+    $mode = $row['Mode'];
+    $modulename = $row['ModuleName'];
+    $credits = $row['Credits'];
+    $semester = $row['Semester'];
+    
+    echo "<TR><TD>$programmecode</TD><TD>$programmename</TD><TD>$mode</TD><TD>$modulename</TD><TD>$credits</TD><TD>$semester</TD>";    
+}
+mysqli_close($conn);
+?>
+
 
